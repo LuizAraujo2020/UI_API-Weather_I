219d130f7150f62532ac2a29a844f702c2a7a3a8
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-01-11 15:33:31 -0300

    Initial Commit

---
+++ UI-Weather-I/UI_Weather_IApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_Weather_IApp.swift
+//  UI-Weather-I
+//
+//  Created by Luiz Araujo on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UI_Weather_IApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
